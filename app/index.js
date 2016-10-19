e82a526c40924b0dcb77058ac35b3300776ba0b0
Luis Santiago <luis.santiagoayala@gmail.com>
2016-10-19 07:46:18 -0700

    Add list of videos.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const express = require('express') 
-const app = express()  
-//const layout = require('./config/views-config')
-const port = Number(process.env.PORT || 3000);
 const exphbs = require('express-handlebars')
 const request = require('request-promise')
+//const layout = require('./config/views-config')
+const app = express()  
+const port = Number(process.env.PORT || 3000);
 const formatter = require('./helpers/requestFormatter') 
 
 app.engine('.hbs', exphbs({  
@@ -35,7 +35,14 @@ app.get('/', (req, res) => {
       //   console.log(videos_data);
       // })
       //TODO: send videos_data to getVideoSource() function
-      var video_sources = ['<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14335548_1051193988330176_850900594_n.mp4" type="video/mp4">']
+      var video_sources = [
+      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14335548_1051193988330176_850900594_n.mp4" type="video/mp4">',
+      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14669536_1760713030860499_7732674590190600192_n.mp4" type="video/mp4">',
+      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14801852_1718398785149252_661271319718920192_n.mp4" type="video/mp4">',
+      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14795120_1089801374468528_2292598764591906816_n.mp4" type="video/mp4">',
+      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14796873_1129518557131557_255216543095324672_n.mp4" type="video/mp4>',
+      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14793103_1704712819850274_7538965195704500224_n.mp4" type="video/mp4>'
+      ]
       var wod = formatter.formatResponse(data);
       res.render('wod', {name: wod,
         source: video_sources[0]
