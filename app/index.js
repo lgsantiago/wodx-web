783f9949e58d6f786ac47c85694b04da5e219c75
Luis Santiago <luis.santiagoayala@gmail.com>
2016-10-19 22:24:18 -0700

    Render multiple videos pulled from api.

--- app/index.js
+++ app/index.js
@@ -27,22 +27,15 @@ app.get('/', (req, res) => {
     json: true
   })
     .then((data) => {
+      // TODO: Request to sound cloud
       // request({
-      //   uri: 'https://api.instagram.com/v1/tags/pushups/media/recent?count=30&client_id=93daf7f27158410b8a8991ab0d8689c0',
+      //   uri: '',
       //   json: true
       // })
       // .then((videos_data)=>{
       //   console.log(videos_data);
       // })
-      //TODO: send videos_data to getVideoSource() function
-      var video_sources = [
-      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14335548_1051193988330176_850900594_n.mp4" type="video/mp4">',
-      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14669536_1760713030860499_7732674590190600192_n.mp4" type="video/mp4">',
-      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14801852_1718398785149252_661271319718920192_n.mp4" type="video/mp4">',
-      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14795120_1089801374468528_2292598764591906816_n.mp4" type="video/mp4">',
-      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14796873_1129518557131557_255216543095324672_n.mp4" type="video/mp4>',
-      '<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14793103_1704712819850274_7538965195704500224_n.mp4" type="video/mp4>'
-      ]
+      var video_sources = ['<source name="video" src="https://scontent-ord1-1.cdninstagram.com/t50.2886-16/14335548_1051193988330176_850900594_n.mp4" type="video/mp4">',]
       var wod = formatter.formatResponse(data);
       res.render('wod', {wod: wod,
         source: video_sources[0]
---
+++ app/views/js/media.js
@@ -0,0 +1,32 @@
+function renderVideos() {
+	var index = 0;
+	var video_sources = [];
+	var url = 'https://wodx.herokuapp.com/api/v1/videos';
+
+	$.ajax({
+		url: url,
+		dataType: 'jsonp',
+		success: function(data){
+			 for (var i = 0; i < data.videos.length; i++) {
+	     		video_sources[i] = data.videos[i];
+	     	}
+		}
+	})
+	.fail(function() {
+	   console.log("Something went wrong with videos ajax request!");
+	 });
+
+	setInterval(function () {
+        
+    	var video = document.getElementById('videos');
+        video.setAttribute("src", video_sources[index]);
+        
+        if(index == video_sources.length -1){
+        	index = 0;
+        }
+
+        index++;
+
+	}, 10*1000);
+
+}
